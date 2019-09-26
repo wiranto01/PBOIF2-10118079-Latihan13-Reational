318c49c4a106514dbeb7e07963c00e2ea72eb287
wiranto01 <55836685+wiranto01@users.noreply.github.com>
2019-09-26 18:32:33 +0700

    Add files via upload

---
+++ src/pboif2/pkg10118079/latihan13/reational/PBOIF210118079Latihan13Reational.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10118079.latihan13.reational;
+
+/**
+ *
+ * @author 
+ * Nama : Wiranto
+ * Kelas : If2
+ * Nim : 10118079
+ */
+public class PBOIF210118079Latihan13Reational {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int bil1 = 10;
+        int bil2 = 20;
+        System.out.println("a == b = " + (bil1 == bil2));
+        System.out.println("a != b = " + (bil1 != bil2));
+        System.out.println("a > b = " + (bil1 > bil2));
+        System.out.println("a < b = " + (bil1 < bil2));
+        System.out.println("b >= a = " + (bil2 >= bil1));
+        System.out.println("b <= a = " + (bil2 <= bil1));
+
+    }
+    
+}
